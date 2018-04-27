464645ae2e6cca782df0aa3e3c3506b80c32cb4b
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-27 01:52:28 +0100

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+
+import HeroImage from './components/HeroImage'
+import Button from "material-ui/Button"
+import AppBar from './components/AppBar'
+import heroImage from './images/mainBanner.jpg'
+
+const AppStyles = {
+	color: '#fff',
+	backgroundImage: `url(${heroImage})`,
+	backgroundRepeat: 'no-repeat',
+	backgroundSize: 'cover',
+	height: '100vh'
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={AppStyles}>
+				<AppBar />
+				<Button variant="raised" color="primary">Welcome</Button>
+			</div>
+		);
+	}
 }
 
 export default App;
